c55f8d4a6504c8f4dcdd0c822036d3dc7083a6e6
LexiphanicStroon <nate_reeves@outlook.com>
2023-08-17 09:48:02 -0500

    modify file structure to be modular

---
+++ src/home.js
@@ -0,0 +1,7 @@
+export default function home() {
+    const home = document.createElement('div');
+    home.textContent = "Hello, I'm home";
+
+    return home;
+
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-const content = document.querySelector('#content');
+const content = document.querySelector("#content");
 
-const main = document.querySelector('#content');
+const main = document.querySelector("#content");
+const topNav = document.createElement("div");
+const homeBtn = document.createElement("p");
+const menuBtn = document.createElement("p");
+const contactBtn = document.createElement("p");
 
-const topNav = document.createElement('div');
+topNav.classList.add("top__nav");
 
+homeBtn.textContent = "Home";
+menuBtn.textContent = "Menu";
+contactBtn.textContent = "Contact";
 
-document.body.appendChild(div);
+topNav.append(homeBtn, menuBtn, contactBtn);
+
+document.body.appendChild(topNav);
+
+const navLinks = document.querySelectorAll("p");
+console.log(navLinks);
+
+function renderPage(content) {
+    
+}
+
+navLinks.forEach((p) => {
+    p.addEventListener("click", () => {console.log('click worked');});
+});
