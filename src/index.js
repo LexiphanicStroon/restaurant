d0dea99cd8ff4f414e653a686964726db91f30c9
LexiphanicStroon <nate_reeves@outlook.com>
2023-08-17 10:17:57 -0500

    implement working module routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import home from "./home";
+import menu from "./menu";
 
 const content = document.querySelector("#content");
 
-const main = document.querySelector("#content");
 const topNav = document.createElement("div");
 const homeBtn = document.createElement("p");
 const menuBtn = document.createElement("p");
@@ -26,7 +26,17 @@ navLinks.forEach((p) => {
     p.addEventListener("click", renderPage);
 });
 
-function renderPage() {
+function renderPage(e) {
     content.innerHTML = '';
-    content.appendChild(home());
+    console.log(e.target.textContent);
+    let route = e.target.textContent.toLowerCase();
+    if (route === "home") {
+        content.appendChild(home());
+    }   else if (route === "menu") {
+        content.appendChild(menu());
+    }
+
+    console.log(route);
+    // const page = document.createElement(route());
+    
 }
---
+++ src/menu.js
@@ -0,0 +1,7 @@
+export default function menu() {
+    const div = document.createElement('div');
+    div.textContent = "Hello, I'm Menu";
+    console.log(div); 
+    
+    return div;
+}
